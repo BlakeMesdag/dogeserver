fa5f0b5f6cc22846bccc37d264e4f3b8ea746b83
Alexander Profeit <alex@profeit.com>
2014-01-23 07:10:03 +0000

    more tests

--- test/models/tip_test.rb
+++ test/models/tip_test.rb
@@ -2,9 +2,11 @@ require 'test_helper'
 
 class TipTest < ActiveSupport::TestCase
   test "tips adjust balances" do
-    assert_difference "small_account.balance", 10 do
-      assert_difference "large_account.balance", -10 do
-        Tip.create(large_account, small_account, 10)
+    assert_difference "Transaction.count", 2 do
+      assert_difference "small_account.balance", 10 do
+        assert_difference "large_account.balance", -10 do
+          Tip.create(large_account, small_account, 10)
+        end
       end
     end
   end
@@ -13,5 +15,25 @@ class TipTest < ActiveSupport::TestCase
     assert_no_difference "Transaction.count" do
       Tip.create(small_account, large_account, 9000)
     end
+  end
+
+  test "nil from doesnt create transactions" do
+    assert_no_difference "Transaction.count" do
+      Tip.create(nil, large_account, 1)
+    end
+  end
+
+  test "nil to doesnt create transactions" do
+    assert_no_difference "Transaction.count" do
+      Tip.create(small_account, nil, 1)
+    end
+  end
+
+  test "errors are present after invalid creation" do
+    expected_errors = { tip: "Failed to create tip" }
+
+    tip = Tip.create(small_account, large_account, 9000)
+
+    assert_equal expected_errors, tip.errors
   end
 end
