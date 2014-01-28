0c06725b7aa08064e0eeea60b22c7ff10d051e4e
Alexander Profeit <alex@profeit.com>
2014-01-28 07:46:34 +0000

    set up fake webs on every test run

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,11 +13,13 @@ class ActiveSupport::TestCase
 
   FakeWeb.allow_net_connect = false
 
-  fake_new_address_response = "0db873ddb14c1465b8edf7d4ca101bf0a4"
-  FakeWeb.register_uri(:get, /.+dogeapi.+get_new_address.+/, body: fake_new_address_response)
+  def setup
+    fake_new_address_response = "0db873ddb14c1465b8edf7d4ca101bf0a4"
+    FakeWeb.register_uri(:get, /.+dogeapi.+get_new_address.+/, body: fake_new_address_response)
 
-  fake_withdrawal_response = '72b635d73a6d054b030e1899257ce0474eefa94c86e26c9dc349f65a5c4c6cf8'
-  FakeWeb.register_uri(:get, /.+dogeapi.+withdraw.+amount.+payment_address/, body: fake_withdrawal_response)
+    fake_withdrawal_response = '72b635d73a6d054b030e1899257ce0474eefa94c86e26c9dc349f65a5c4c6cf8'
+    FakeWeb.register_uri(:get, /.+dogeapi.+withdraw.+amount.+payment_address/, body: fake_withdrawal_response)
+  end
 
   def small_account
     accounts(:small)
