a2420a43aa1e147cb3dc03625cde70d190caff4a
Alexander Profeit <alex@profeit.com>
2014-01-28 18:01:25 +0000

    dry dogeapi

--- app/models/doge_api.rb
+++ app/models/doge_api.rb
@@ -7,27 +7,24 @@ module DogeAPI
   base_uri "https://www.dogeapi.com/wow/?api_key=#{Settings::KEYS['DOGEAPI_API_KEY']}&a="
 
   def get_new_address(address_label)
-    query    = "get_new_address&address_label=#{address_label}"
-    response = get(query)
-    dog_log(query, response.code)
-    response.parsed_response
+    get_with_logging("get_new_address&address_label=#{address_label}")
   end
 
   def get_address_received(deposit_address)
-    query    = "get_address_received&payment_address=#{deposit_address}"
-    response = get(query)
-    dog_log(query, response.code)
-    response.parsed_response
+    get_with_logging("get_address_received&payment_address=#{deposit_address}")
   end
 
   def withdraw(to, amount)
-    query    = "withdraw&amount=#{amount}&payment_address=#{to}"
+    get_with_logging("withdraw&amount=#{amount}&payment_address=#{to}")
+  end
+
+  private
+
+  def get_with_logging(query)
     response = get(query)
     dog_log(query, response.code)
     response.parsed_response
   end
-
-  private
 
   def dog_log(message, code)
     Rails.logger.info("[DogAPI] GET #{message} -> #{code}")
