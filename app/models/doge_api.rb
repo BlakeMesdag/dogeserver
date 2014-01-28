466af3c884b19c9e1849f239f4f0bc96ea3a6adf
Alexander Profeit <alex@profeit.com>
2014-01-28 07:44:39 +0000

    log dognap calls

--- app/models/doge_api.rb
+++ app/models/doge_api.rb
@@ -1,18 +1,35 @@
 module DogeAPI
+  class UnexpectedResponse < StandardError; end
+
   include HTTParty
   extend self
 
   base_uri "https://www.dogeapi.com/wow/?api_key=#{Settings::KEYS['DOGEAPI_API_KEY']}&a="
 
   def get_new_address(address_label)
-    get("get_new_address&address_label=#{address_label}").parsed_response
+    query    = "get_new_address&address_label=#{address_label}"
+    response = get(query)
+    dog_log(query, response.code)
+    response.parsed_response
   end
 
   def get_address_received(deposit_address)
-    get("get_address_received&payment_address=#{deposit_address}").parsed_response
+    query    = "get_address_received&payment_address=#{deposit_address}"
+    response = get(query)
+    dog_log(query, response.code)
+    response.parsed_response
   end
 
   def withdraw(to, amount)
-    get("withdraw&amount=#{amount}&payment_address=#{to}")
+    query    = "withdraw&amount=#{amount}&payment_address=#{to}"
+    response = get(query)
+    dog_log(query, response.code)
+    response.parsed_response
+  end
+
+  private
+
+  def dog_log(message, code)
+    Rails.logger.info("[DogAPI] GET #{message} -> #{code}")
   end
 end
